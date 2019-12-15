587fbb05bd0be5ca466bcc2450238c9fadb428fe
Michal Stawinski <michal.stawinski@gmail.com>
2019-12-15 18:01:19 +0100

    Renamed some columns

--- tinymix/models.py
+++ tinymix/models.py
@@ -7,6 +7,7 @@ from django.db import models
 
 class Config(models.Model):
     name = models.CharField(max_length=200)
+    device_id = models.IntegerField('device id')
     created_date = models.DateTimeField('date created')
 
     def __str__(self):
@@ -19,17 +20,17 @@ class Config(models.Model):
 class Control(models.Model):
     config = models.ForeignKey(Config, on_delete=models.CASCADE)
     control_name = models.CharField('control name', max_length=200)
-    control_index = models.IntegerField('control index')
+    control_id = models.IntegerField('control id')
     value_current = models.ForeignKey('Value', on_delete=models.CASCADE)
 
     def __str__(self):
-        return "<" + str(self.control_index) + ": " + self.control_name + " current=" + str(self.value_current) + ">"
+        return "<" + str(self.control_id) + ": " + self.control_name + " current=" + str(self.value_current) + ">"
 
 
 class Value(models.Model):
     parent = models.ForeignKey(Control, on_delete=models.CASCADE)
     value_name = models.CharField('value name', max_length=200)
-    value_index = models.IntegerField('value index')
+    value_id = models.IntegerField('value id')
 
     def __str__(self):
-        return "<" + str(self.value_index) + ": " + self.value_name + ">"
+        return "<" + str(self.value_id) + ": " + self.value_name + ">"

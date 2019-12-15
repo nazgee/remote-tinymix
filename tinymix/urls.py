4b14389e76169800cac60e3931c8a546ef6727c5
Michal Stawinski <michal.stawinski@gmail.com>
2019-12-15 19:36:55 +0100

    Forms are working

--- tinymix/urls.py
+++ tinymix/urls.py
@@ -6,10 +6,12 @@ app_name = 'tinymix'
 urlpatterns = [
     # ex: /tinymix/
     path('', views.index, name='index'),
-    # ex: /tinymix/5/
-    path('config/<int:config_id>/', views.detail, name='detail'),
-    # ex: /tinymix/5/results/
+    # ex: /tinymix/config/5
+    path('config/<int:config_id>/', views.config, name='config'),
+    # ex: /tinymix/control/5
     path('control/<int:control_id>', views.control, name='control'),
-    # ex: /tinymix/5/vote/
-    path('edit/<int:value_id>', views.edit, name='edit'),
+    # ex: /tinymix/control_edit/5
+    path('control_edit/<int:control_id>', views.control_edit, name='control_edit'),
+    # ex: /tinymix/value/5
+    path('value/<int:value_id>', views.value, name='value'),
 ]
--- tinymix/views.py
+++ tinymix/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.urls import reverse
 
 from tinymix.models import Config, Control, Value
 
@@ -13,18 +14,38 @@ def index(request):
     return render(request, 'tinymix/index.html', context)
 
 
-def detail(request, config_id):
+def config(request, config_id):
     config = get_object_or_404(Config, pk=config_id)
-    return render(request, 'tinymix/detail.html', {'config': config})
+    return render(request, 'tinymix/config.html', {'config': config})
 
 
 def control(request, control_id):
     # response = "You're looking at control %s."
     # return HttpResponse(response % control_id)
-    control = get_object_or_404(Control, pk=control_id)
-    return render(request, 'tinymix/control.html', {'control': control})
+    c = get_object_or_404(Control, pk=control_id)
+    return render(request, 'tinymix/control.html', {'control': c})
 
 
-def edit(request, value_id):
-    value = get_object_or_404(Value, pk=value_id)
-    return render(request, 'tinymix/value.html', {'value': value})
+def control_edit(request, control_id):
+    ctrl = get_object_or_404(Control, pk=control_id)
+    try:
+        val = ctrl.value_set.get(pk=request.POST['newvalue'])
+    except (KeyError, Value.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'tinymix/control.html', {
+            'control': ctrl,
+            'error_message': "You didn't select a value.",
+        })
+    else:
+        ctrl.value_current = val
+        ctrl.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('tinymix:config', args=(ctrl.config.pk,)))
+        # return render(request, 'tinymix/control_edit.html', {'value': val})
+
+
+def value(request, value_id):
+    v = get_object_or_404(Value, pk=value_id)
+    return render(request, 'tinymix/value.html', {'value': v})
